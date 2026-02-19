069e26c4e64b4dda88d916ef8ee7b580b527b422
Saranya Nayak <san00kam1@proton.me>
2026-02-19 21:13:19 +0530

    Enhance explore route to handle POST requests; validate email and display user input in explore template

--- app.py
+++ app.py
@@ -7,8 +7,20 @@ def home():
    
     return render_template("home.html")
 
-@app.route('/explore') #  http://192.168.1.49:5001/explore
+@app.route('/explore', methods=['GET', 'POST']) #  http://192.168.1.49:5001/explore
 def explore():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        if '@' in email and '.' in email:
+            print(f"\n\n\nUsername: {username}, Email: {email}, Password: {password}")
+            return render_template("explore.html", username=username, email=email, password=password)
+        else:
+            print("\n\n\nInvalid email address. Please enter a valid email.")
+
+
     return render_template("explore.html")
 
 
