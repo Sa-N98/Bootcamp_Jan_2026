e2aefce48d19f7465cbe261c6e2d44a233899aa1
Saranya Nayak <san00kam1@proton.me>
2026-02-19 20:51:48 +0530

    Add my_bookings route and template; refactor home and explore templates

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/') # / == http://192.168.1.49:5001/
 def home():
+   
     return render_template("home.html")
 
 @app.route('/explore') #  http://192.168.1.49:5001/explore
 def explore():
     return render_template("explore.html")
+
+
+@app.route('/my_bookings') #  http://192.168.1.49:5001/my_bookings
+def my_bookings():
+    return render_template("my_bookings.html")
 
 app.run(debug=True, host='0.0.0.0', port=5001)
